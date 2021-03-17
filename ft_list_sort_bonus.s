6128e6059d2c3f19784011e3d8ac69478e8d983b
LeoBourret <bourretleo@hotmail.fr>
2021-03-17 13:32:54 +0100

    Hello libasm

--- ft_list_sort_bonus.s
+++ ft_list_sort_bonus.s
@@ -3,13 +3,13 @@ section		.text
 
 				;rdi = **begin_list rsi = int(*cmp)
 ft_list_sort:
-				mov		rdx, [rdi]		;rdx = current
-				push	r12				;r12 = tmp for swap
-				xor		r12, r12
-				push	rax
-				xor		rax, rax
-				cmp		qword[rdx + 8], 0
-				je		return
+				push	rdx
+				push	r12
+				mov		r12, [rdi]			;r12 = save begin
+				cmp		rdi, 0				;begin == NULL ?
+				je		pop_register
+				cmp		rsi, 0				;int(cmp)() == NULL ?
+				je		pop_register
 				jmp		compare
 restart:
 				mov		rdx, rdi
@@ -17,6 +17,10 @@ restart:
 
 list_inc:
 				mov		rdx, [rdx + 8]
+
+set_compare:
+				cmp		[rdi], 0			;*begin == NULL ?
+				jz		return
 
 compare:
 				cmp		byte[rdx + 8], 0
@@ -40,7 +44,11 @@ swap:
 				mov		rdx, [rsi]
 				mov		rsi, [r12]
 				jmp		restart
+
 return:
+				mov		[rdi], r12
+
+pop_register:
 				pop		r12
 				pop		rax
 				ret
